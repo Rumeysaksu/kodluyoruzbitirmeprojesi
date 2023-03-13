038dc65e9fec0cec63518299e969d177ed0876ee
Rümeysa AKSU <124360198+Rumeysaksu@users.noreply.github.com>
2023-03-13 03:00:49 +0300

    Create soru.js

---
+++ test.i/js/soru.js
@@ -0,0 +1,67 @@
+let questions = [
+    {
+        soru_num: 1,
+        question: "Aşağıdakilerden hangisi mayoz ve mitozun ortak özelliklerinden biri değildir?",
+        cevap: "D) Çekirdekçik ve çekirdek zarının yeniden oluşması.",
+        seçenekler: [
+        "A) Sentromer bölünmesi.",
+        "B) Kardeş kromatitlerin ekvator düzlemine dizilmesi ve ayrılması.",
+        "C) Kromozom sayısı değişmez.",
+        "D) Çekirdekçik ve çekirdek zarının yeniden oluşması.",
+        "E) İğ ipliklerinin kromatitlere kinetokor bölgesine bağlanması."
+     
+       ]
+  },
+    {
+    soru_num: 2,
+    question: "Vejetatif üreme şekillerinden biri değildir?",
+    cevap: "A) Sporla Üreme",
+    seçenekler: [
+        "A) Sporla Üreme",
+        "B) Rizomla Üreme",
+        "C) Doku Kültürü Tekniği",
+        "D) Çelikle Üreme",
+        "E) Sürünücü Gövdeyle Üreme"
+      ]
+  },
+    {
+    soru_num: 3,
+    question: "Aşağıdakilerden hangisi eşeysiz üreme çeşitlerinden biri değildir?",
+    cevap: "B) Rizomla Üreme",
+    seçenekler: [
+        
+        "A) Sporla Üreme",
+        "B) Rizomla Üreme",
+        "C) Partenogenez",
+        "D) Vejetatif Üreme",
+        "E) Rejenerasyonla Üreme"
+      ]
+  },
+    {
+    soru_num: 4,
+    question: "2n =18+XX kromozom formüllü bir kedinin hücresi mitoz bölünme geçirmektedir buna göre metafaz evresindeki kromatit sayısı kaçtır?",
+    cevap: "E) 40",
+    seçenekler: [
+        "A) 10",
+        "B) 9",
+        "C) 18",
+        "D) 20",
+        "E) 40"
+      
+    ]
+  },
+    {
+    soru_num: 5,
+    
+    question: "Mitoz bölünme sırasında mikroskop ile görüntülenen hayvan hücresinin evrelerini sıralaması hangisinde doğrudur?",
+    cevap: "E) I-III-II-IV",
+    seçenekler: [
+        "A) I-II-III-IV",
+        "B) II-III-I-IV",
+        "C) IV-I-II-III",
+        "D) IV-II-III-I",
+        "E) I-III-II-IV"
+    ]
+  },
+  
+];
